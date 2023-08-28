cb5ef13c5d525b631236c4eca3e980261d3ac37d
Simon Stipcich <simon.stipcich@derivco.co.za>
2023-08-28 09:04:59 +0200

    feat:some cleanup.

--- src/kafka_consumer_emulator.py
+++ src/kafka_consumer_emulator.py
@@ -1,17 +1,23 @@
 # from multiprocessing import JoinableQueue, process
 from process_cmd import ProcessCmd
 import multiprocessing as mp
+from cmd_provider import CmdProvider
 
 
-class KafkaConsumerEmulator:
-    def __init__(self, queue: mp.JoinableQueue):
-        self._queue = queue
+# class KafkaConsumerEmulator:
+#     def __init__(self, queue: mp.JoinableQueue):
+#         self._queue = queue
 
-    def consume(self):
-        current_process = mp.current_process()
-        process_id = current_process.pid
-        print("KafkaConsumerEmulator", "consume()", f"process: {process_id}")
-        for message in range(10):
-            # todo: push cmds...
-            # self._queue.put(f"Command {message}")
-            self._queue.put(ProcessCmd())
+#     def consume(self):
+#         current_process = mp.current_process()
+#         process_id = current_process.pid
+#         print("KafkaConsumerEmulator", "consume()", f"process: {process_id}")
+#         for message in range(10):
+#             # todo: push cmds...
+#             # self._queue.put(f"Command {message}")
+#             self._queue.put(ProcessCmd())
+
+
+class KafkaConsumerEmulator(CmdProvider):
+    def provide(self) -> [ProcessCmd]:
+        return [ProcessCmd() for _ in range(10)]
--- src/process_manager.py
+++ src/process_manager.py
@@ -1,37 +1,36 @@
 from cmd_processor import CmdProcessor
 from process_cmd import ProcessCmd
-from cmd_provider import CmdProvider
+from cmd_provider import CmdProvider, CmdProviderManager
 from kafka_consumer_emulator import KafkaConsumerEmulator
 from multiprocessing import JoinableQueue, Process
 from process_metadata_service import ProcessMetadataService
 
 
 class ProcessManager:
-    def __init__(self, cmd_processor: CmdProcessor, cmd_provider: CmdProvider):
-        self._queue = JoinableQueue()
-        pass
+    def __init__(self, cmd_processor: CmdProcessor, queue: JoinableQueue):
+        self._cmd_processor = cmd_processor
+        self._queue = queue
 
-    def start(self):
-        pass
+    def manage(self):
+        while True:
+            msg = self._queue.get()
+            if msg is None:
+                break
 
-    def stop(self):
-        pass
-
-    def join(self):
-        pass
+            print(f"Processing message {msg.idempotency_id}")
+            self._cmd_processor.process(msg)
+            self._queue.task_done()
 
 
-def message_worker(queue, cmd_processor: CmdProcessor):
-    while True:
-        pid = ProcessMetadataService().pid()
-        print(f"message_worker() / pid:{pid}")
-        msg = queue.get()
-        if msg is None:
-            break
-        # cmd = ProcessCmd()
-        print(f"Processing message {msg.idempotency_id}")
-        cmd_processor.process(msg)
-        queue.task_done()
+# def message_worker(queue, cmd_processor: CmdProcessor):
+#     while True:
+#         msg = queue.get()
+#         if msg is None:
+#             break
+
+#         print(f"Processing message {msg.idempotency_id}")
+#         cmd_processor.process(msg)
+#         queue.task_done()
 
 
 def main():
@@ -39,24 +38,27 @@ def main():
 
     queue = JoinableQueue()
     cmd_processor = CmdProcessor()
-    consumer_manager = KafkaConsumerEmulator(queue)
+    cmd_provider = KafkaConsumerEmulator()
+    cmd_provider_manager = CmdProviderManager(cmd_provider, queue)
+    process_manager = ProcessManager(cmd_processor, queue)
 
-    consumers = []
+    providers = []
 
     for _ in range(num_consumers):
-        consumer = Process(target=consumer_manager.consume)
-        consumers.append(consumer)
-        consumer.start()
+        provider = Process(target=cmd_provider_manager.manage)
+        providers.append(provider)
+        provider.start()
 
     workers = []
 
     for _ in range(num_consumers):
-        worker = Process(target=message_worker, args=(queue, cmd_processor))
+        # worker = Process(target=message_worker, args=(queue, cmd_processor))
+        worker = Process(target=process_manager.manage)
         workers.append(worker)
         worker.start()
 
-    for consumer in consumers:
-        consumer.join()
+    for provider in providers:
+        provider.join()
 
     for _ in range(num_consumers):
         queue.put(None)  # Signal workers to exit
