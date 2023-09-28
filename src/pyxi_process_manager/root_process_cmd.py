89b7b2aadf032c41316c82d281b8133bcb656e29
stiproot <stipcich.simon@gmail.com>
2023-09-28 17:07:51 +0200

    feat:root cmd.

---
+++ src/pyxi_process_manager/root_process_cmd.py
@@ -0,0 +1,20 @@
+class RootCmd:
+    def __init__(
+        self,
+        cmd_data: dict,
+        cmd_metadata: dict,
+    ):
+        self.cmd_data = cmd_data
+        self.cmd_metadata = cmd_metadata
+
+    def __cmd_post_op__(self) -> dict:
+        return self.cmd_metadata["cmd_post_op"]
+
+    def __cmd_post_op_enrichment__(self) -> dict:
+        return self.__cmd_post_op__().get("enrichment", {})
+
+    def _idempotency_id_(self) -> str:
+        return self.cmd_metadata["idempotency_id"]
+
+    def _cmd_post_op_enrichement_map_(self) -> dict:
+        return self.__cmd_post_op_enrichment__().get("add_property_map", [])
